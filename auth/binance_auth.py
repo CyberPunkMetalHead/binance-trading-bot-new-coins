d4dadc9923e8633476fa1c64c085a7a6248c90ac
stevenhgs <48204700+stevenhgs@users.noreply.github.com>
2021-09-03 12:32:58 +0200

    Added multithreading to speed up the interval between checking if new coins are added. (#16)

--- auth/binance_auth.py
+++ auth/binance_auth.py
@@ -8,4 +8,4 @@ def load_binance_creds(file):
     with open(file) as file:
         auth = yaml.load(file, Loader=yaml.FullLoader)
 
-    return Client(api_key = auth['binance_api'], api_secret = auth['binance_secret'], tld = "com" if 'binance_tld' not in auth else auth['binance_tld'])
+    return Client(api_key = auth['binance_api'], api_secret = auth['binance_secret'])
